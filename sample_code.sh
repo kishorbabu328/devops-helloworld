da582864ee574419c42a9db87fc7ace2a61d17ac
kishorbabu328 <71311246+kishorbabu328@users.noreply.github.com>
2020-10-20 09:35:26 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -4,6 +4,8 @@
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
+cho "This is an example of a new script"
+cho "This is an new example of a script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
